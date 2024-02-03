a9b48b4018697de97e94d1b2213f974e5f1247c7
Surafel Araya <surafelay@gmail.com>
2024-02-03 23:01:24 +0300

    Main content Layouting

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,19 +2,19 @@ import Card from "./Card";
 
 const Main = () => {
   return (
-    <div className="border">
-      <h1>Musics</h1>
-      <div className="row">
-        <div className="col-lg-6 col-xl-4">
+    <div className="">
+      <h1 className="mb-3">Musics</h1>
+      <div className="row justify-content-evenly">
+        <div className="col-lg-6 col-xl-4 mb-4">
           <Card />
         </div>
-        <div className="col-lg-6 col-xl-4">
+        <div className="col-lg-6 col-xl-4 mb-4">
           <Card />
         </div>
-        <div className="col-lg-6 col-xl-4">
+        <div className="col-lg-6 col-xl-4 mb-4">
           <Card />
         </div>
-        <div className="col-lg-6 col-xl-4">
+        <div className="col-lg-6 col-xl-4 mb-4">
           <Card />
         </div>
       </div>
