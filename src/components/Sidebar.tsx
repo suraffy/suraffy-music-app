35a035ec8da8c415c90410bf1dace909dc8ffde9
Surafel Araya <surafelay@gmail.com>
2024-02-03 21:19:48 +0300

    Buid a Sidebar component

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,18 @@
+const Sidebar = () => {
+  return (
+    <div className="border">
+      <h2>Genres</h2>
+      <ul>
+        <li>Pop</li>
+        <li>Rock</li>
+        <li>Hip Hop</li>
+        <li>Jazz</li>
+        <li>Blues</li>
+        <li>Electronic</li>
+        <li>Classical</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
