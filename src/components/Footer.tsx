11003664e7340b592d285d23ba015a83db4a1992
Surafel Araya <surafelay@gmail.com>
2024-02-04 22:30:32 +0300

    Build a Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <footer className="container mt-5">
+      <hr />
+      <span className="d-block text-center  mt-1 mb-3">
+        Designed and Developed by{" "}
+        <a href="https://suraffy.netlify.app" target="blank">
+          Surafel Araya
+        </a>
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
