9ca4bc96d20e3f491a6f629199eaed91cd551203
Surafel Araya <surafelay@gmail.com>
2024-02-04 19:47:25 +0300

    Handle form values using Formik

--- src/components/common/Form.tsx
+++ src/components/common/Form.tsx
@@ -1,78 +1,133 @@
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { MusicState } from "../../redux/musicSlice";
+
+const schema = Yup.object().shape({
+  title: Yup.string().required("Song title is required"),
+  artist: Yup.string().required("Artist name is required"),
+  genre: Yup.string().required("Song genre is required"),
+  album: Yup.string(),
+});
+
 interface Props {
-  music: {
-    id: number;
-    title: string;
-    artist: string;
-    genre: string;
-    album: string;
-  };
+  music: MusicState;
+
+  onCloseModal: (value: MusicState | null) => void;
 }
 
-const Form = ({ music }: Props) => {
+const Form = ({ music, onCloseModal }: Props) => {
+  const formik = useFormik({
+    initialValues: {
+      id: music.id,
+      title: music.title,
+      artist: music.artist,
+      genre: music.genre,
+      album: music.album,
+    },
+    onSubmit: (value: MusicState) => {
+      onCloseModal(value);
+    },
+
+    validationSchema: schema,
+  });
+
   return (
-    <form>
-      <div className="form-group mb-2">
-        <label htmlFor="song-title">Song Title</label>
-        <input
-          type="text"
-          className="form-control"
-          id="song-title"
-          name="title"
-          defaultValue={music.title}
-          autoFocus
-        />
-        <small className="form-text text-danger">
-          Error Message appears here
-        </small>
-      </div>
-
-      <div className="form-group mb-3">
-        <select className="form-control">
-          <option>Select Genre</option>
-          <option value={"pop"}>Pop</option>
-          <option value={"jazz"}>Jazz</option>
-          <option value={"blues"}>Blues</option>
-          <option value={"rock"}>Rock</option>
-          <option value={"hiphop"}>Hip Hop</option>
-          <option value={"electronic"}>Electronic</option>
-          <option value={"classical"}>Classical</option>
-        </select>
-
-        <small className="form-text text-danger">
-          Error Message appears here
-        </small>
-      </div>
-
-      <div className="row">
-        <div className="form-group mb-2 col">
-          <label htmlFor="artist-name">Artist Name</label>
+    <div className="">
+      <form onSubmit={formik.handleSubmit}>
+        <div className="form-group mb-2">
+          <label htmlFor="song-title">Song Title</label>
           <input
             type="text"
             className="form-control"
-            id="artist-name"
-            name="artist"
-            defaultValue={music.artist}
+            id="song-title"
+            name="title"
+            value={formik.values.title}
+            onChange={formik.handleChange}
+            autoFocus
           />
-          <small className="form-text text-danger">
-            Error Message appears here
-          </small>
+          {formik.errors.title && (
+            <small className="form-text text-danger">
+              {formik.errors.title}
+            </small>
+          )}
         </div>
 
-        <div className="form-group mb-2 col">
-          <label htmlFor="song-album">Album</label>
-          <input
-            type="text"
+        <div className="form-group mb-3">
+          <select
             className="form-control"
-            id="song-album"
-            name="album"
-            defaultValue={music.album}
-          />
-          <small className="form-text text-danger">
-            Error Message appears here
-          </small>
+            name="genre"
+            value={"formik.values.genre"}
+            onChange={formik.handleChange}
+          >
+            <option>Select Genre</option>
+            <option value={"pop"}>Pop</option>
+            <option value={"jazz"}>Jazz</option>
+            <option value={"blues"}>Blues</option>
+            <option value={"rock"}>Rock</option>
+            <option value={"hiphop"}>Hip Hop</option>
+            <option value={"electronic"}>Electronic</option>
+            <option value={"classical"}>Classical</option>
+          </select>
+
+          {formik.errors.genre && (
+            <small className="form-text text-danger">
+              {formik.errors.genre}
+            </small>
+          )}
         </div>
-      </div>
-    </form>
+
+        <div className="row">
+          <div className="form-group mb-2 col">
+            <label htmlFor="artist-name">Artist Name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="artist-name"
+              name="artist"
+              value={formik.values.artist}
+              onChange={formik.handleChange}
+            />
+            {formik.errors.artist && (
+              <small className="form-text text-danger">
+                {formik.errors.artist}
+              </small>
+            )}
+          </div>
+
+          <div className="form-group mb-2 col">
+            <label htmlFor="song-album">Album</label>
+            <input
+              type="text"
+              className="form-control"
+              id="song-album"
+              name="album"
+              value={formik.values.album}
+              onChange={formik.handleChange}
+              placeholder="Single"
+            />
+            {formik.errors.album && (
+              <small className="form-text text-danger">
+                {formik.errors.album}
+              </small>
+            )}
+          </div>
+        </div>
+
+        <div className="modal-footer justify-content-center">
+          <button
+            type="button"
+            className="btn btn-secondary"
+            onClick={() => onCloseModal(null)}
+          >
+            Discard
+          </button>
+          <button className="btn btn-warning" type="submit">
+            Update music
+          </button>
+        </div>
+      </form>
+      <p>What is wrong</p>
+    </div>
   );
 };
 
