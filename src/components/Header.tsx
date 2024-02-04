d06b7e955ff95a740cf5c8615278fbf361e1247a
Surafel Araya <surafelay@gmail.com>
2024-02-04 22:57:57 +0300

    Implement NavLink

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { NavLink } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { search } from "../redux/musicSlice";
 import HeadPhoneSVG from "../assets/headphone.svg";
@@ -16,14 +17,14 @@ const Header = () => {
   return (
     <div className="border-bottom py-2 mb-4 sticky-top bg-white">
       <header className="container d-flex flex-wrap justify-content-between">
-        <a
-          href="/"
+        <NavLink
+          to="/music"
           className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none"
           style={{ outlineColor: "blue" }}
         >
           <img src={HeadPhoneSVG} alt="" height={"50px"} />
           <span className="fs-4 px-2 h1 text-primary">Music App</span>
-        </a>
+        </NavLink>
 
         <ul className="nav nav-pills pt-2">
           <form
@@ -41,14 +42,25 @@ const Header = () => {
 
           <div className="d-flex">
             <li className="nav-item">
-              <a href="#" className="nav-link active" aria-current="page">
+              <NavLink
+                to="/music"
+                className={({ isActive }) =>
+                  isActive ? "active nav-link" : "nav-link"
+                }
+                aria-current="page"
+              >
                 Music
-              </a>
+              </NavLink>
             </li>
             <li className="nav-item">
-              <a href="#" className="nav-link">
+              <NavLink
+                to="/statistics"
+                className={({ isActive }) =>
+                  isActive ? "active nav-link" : "nav-link"
+                }
+              >
                 Statistics
-              </a>
+              </NavLink>
             </li>
           </div>
         </ul>
