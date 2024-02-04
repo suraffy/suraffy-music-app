e3357c837cdc31c9802dff90ad085f2a1457ab3b
Surafel Araya <surafelay@gmail.com>
2024-02-04 23:02:31 +0300

    Setup a NotFound page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,9 @@
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div>
+      <h2 className="display-3 text-center mt-5 ">404 Not Found</h2>
+    </div>
+  );
 };
 
 export default NotFound;
