9e1ef0d11f8dcd25380240edd002f7de38b4fe21
Surafel Araya <surafelay@gmail.com>
2024-02-08 09:24:27 +0300

    Sidebar: add emoji

--- src/components/common/StaticCard.tsx
+++ src/components/common/StaticCard.tsx
@@ -17,7 +17,7 @@ const StaticCard = ({ header, count, description }: Props) => {
       className="card border-light mt-3 shadow-sm"
       style={{ maxWidth: "18rem" }}
     >
-      <div className="card-header">{header}</div>
+      <div className="card-header">🎧 {header}</div>
       <div className="card-body">
         <h5 className="card-title display-4">{count}</h5>
         <p className="card-text">{description}</p>
